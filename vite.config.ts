2e1103f8fd399d7b5fdabd3a1306877f0c216470
t1m0n <t1m0n.TR@gmail.com>
2025-03-21 16:29:29 +0300

    Added `base` to the vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react-swc';
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [react()],
+  base: 'login-form',
   resolve: {
     alias: {
       ':ui': path.resolve(__dirname, './src/ui'),
